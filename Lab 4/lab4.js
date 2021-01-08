2d6ed67730c24e86ec6edd21b01153845ab14187
Asia A <52732384+AsiaAnderson1@users.noreply.github.com>
2021-01-07 20:41:40 -0500

    Update to Quarterly sales application

--- Lab 4/lab4.js
+++ Lab 4/lab4.js
@@ -57,40 +57,6 @@ function CalculateCommission()
     console.log("Top sales person: " + topSalesPerson + " with a Top Commission of: " + topCommission );
     
  
-    
-    
- 
-
-
-    //Your code goes here
-    /*
-    Here's the pseudocode in case you are wondering how to get started:
-
-    1. Save salesperson names in an array
-
-    2. Save December sales amount in an array
-
-    3. Save January sales amount in an array
-
-    4. Save February sales amount in an array
-
-    5. In a "For" loop, for each person
-
-        Calculate Total sales = December sale + January sale + February sale
-        
-        Calculate commission: 
-        if total sales is greater than 30000 then commission = total sales * 0.08 
-        If total sales is greater than 15000).....etc
-        Continue the if condition for the remaining as specified in the requirement for commission %
-        
-        Calculate average commission:
-        Avg commission = Total commission / 3
-        Display sales person name, commission and average commission
-        Store the commission to a "topCommission" variable.
-        Each time compare topCommission to commission of current salesperson 
-        to determine if this is the top commission
-
-    6. Outside the "For" loop, display topCommission and topSalesperson name.
-*/
+   
 
 }
